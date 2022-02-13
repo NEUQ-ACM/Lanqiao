4ee7546b46a87f25936e2b350bf55ce841733bc6
xuanling233 <92764484+xuanling233@users.noreply.github.com>
2022-02-13 18:14:23 +0800

    Add files via upload

---
+++ 周静妮/数列求和.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a1, a2, a3, a4;
+	a1 = a2 = a3 = 1;
+	for (int i = 4; i <= 20190324; i++)
+	{
+		a4 = a1 + a2 + a3;
+		a4 %= 10000;
+		a1 = a2;
+		a2 = a3;
+		a3 = a4;
+	}
+	cout << a4;
+	return 0;
+}
+
