c0e871a4a1186300227e76dfa753dea7ffb18dcd
jiaranshiwode <92633449+jiaranshiwode@users.noreply.github.com>
2022-02-13 16:24:23 +0800

    Add files via upload

---
+++ 邢大路/C.货物摆放.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n=2021041820210418;
+long long num[200000005];
+int main(){
+    int count=0;
+    for(long long i=1;i*i<=n;i++){
+    	if(n%i==0){
+    		num[count++]=i;
+    		num[count++]=n/i;
+		}
+	}
+	long long ans=0;
+	for(int i=0;i<count;i++){
+		for(int j=0;j<count;j++){
+			for(int k=0;k<count;k++){
+			if(num[i]*num[j]*num[k]==n)ans++;	
+			}
+		}
+	}
+	cout<<ans;
+	return 0;
+}
