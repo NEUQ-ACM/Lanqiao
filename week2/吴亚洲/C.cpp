244eb3620bee1b9bba188bd1c4a75e20052350d4
unknown <1810531233@qq.com>
2022-02-20 16:46:25 +0800

    upload bianweiyi's homework

--- week2/吴亚洲/C.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	long long x=0;
-	for(int i=1;i<=38;i++)
-	x+=i;
-	cout<<x+20;
-}
--- week2/彭政达/试题E：平面分割.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    int n=20,m=20;
-    int ans=1+(n*(n+1))/2+(m*(2*n+2*n+2*(m-1)))/2;
-    cout<<ans<<endl;
-}
